51c45ee9d891be9e19d9de375f60f9eee44b19da
FabienDeshayes <fabien.deshayes@gmail.com>
2016-04-18 16:19:04 +0100

    Show fidsi on tiles

--- src/components/hex-grid.js
+++ src/components/hex-grid.js
@@ -3,20 +3,23 @@ import { connect } from 'react-redux'
 import _ from 'lodash'
 import Tile from './tile'
 
+const MAP_WIDTH = 800
+    , MAP_HEIGHT = 600
+
 class HexGrid extends React.Component {
 
   render() {
     const grid = this.props.grid
     const tiles = _.flatten(
       _.map(grid, function(row, x) {
-        return _.map(row, function(col, y) {
-          return <Tile key={x + ',' + y} x={x} y={y} />
+        return _.map(row, function(data, y) {
+          return <Tile key={x + ',' + y} x={x} y={y} data={data} />
         })
       })
     )
 
     return (
-      <svg>
+      <svg width={ MAP_WIDTH } height={ MAP_HEIGHT }>
       { tiles }
       </svg>
     )
--- src/components/tile.js
+++ src/components/tile.js
@@ -8,7 +8,7 @@ const SIZE = 30
 export default class Tile extends React.Component {
 
   render() {
-    const { x, y } = this.props
+    const { x, y, data } = this.props
 
     let points = [
       [0          , .25 * HEIGHT]
@@ -19,11 +19,28 @@ export default class Tile extends React.Component {
     , [0          , .75 * HEIGHT]
     ]
 
-    points = _.map(points, function(tuple) { return (tuple[0] + x * WIDTH + y % 2 * 1/2 * WIDTH) + ',' + (tuple[1] + y * 3/4 * HEIGHT) }).join(' ')
+    const origin = {
+      x: x * WIDTH  + y % 2 * 1/2 * WIDTH
+    , y: y * 3/4 * HEIGHT
+    }
+
+    points = _.map(points, function(tuple) { return (origin.x + tuple[0]) + ',' + (origin.y + tuple[1]) }).join(' ')
+
+    const textStyle = {
+      fontFamily: 'Consolas',
+      fontSize  : '12px',
+      fill      : '#ffffff',
+      stroke    : '#000000'
+    }
 
     return (
       <g>
-        <polygon points={ points } fill="red" stroke="blue"/>
+        <polygon points={ points } fill="rgba(255,0,0,.3)" stroke="black"/>
+        <text x={ origin.x + 1 * WIDTH / 3 } y={ origin.y + 1 * HEIGHT / 3 } style={ textStyle }>{ data.fidsi[0] }</text>
+        <text x={ origin.x + 2 * WIDTH / 3 } y={ origin.y + 1 * HEIGHT / 3 } style={ textStyle }>{ data.fidsi[1] }</text>
+        <text x={ origin.x + 1 * WIDTH / 3 } y={ origin.y + 2 * HEIGHT / 3 } style={ textStyle }>{ data.fidsi[2] }</text>
+        <text x={ origin.x + 2 * WIDTH / 3 } y={ origin.y + 2 * HEIGHT / 3 } style={ textStyle }>{ data.fidsi[3] }</text>
+        <text x={ origin.x + 1 * WIDTH / 2 } y={ origin.y + 4 * HEIGHT / 5 } style={ textStyle }>{ data.fidsi[4] }</text>
       </g>
     )
   }
